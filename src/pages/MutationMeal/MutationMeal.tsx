0be4ba23cff5fd69e00046e283a7ec4a155c18ce
Sultan Mukhtarov <suli419956@gmail.com>
2024-07-07 18:28:09 +0600

    Created a form with two-way binding.

--- src/pages/MutationMeal/MutationMeal.tsx
+++ src/pages/MutationMeal/MutationMeal.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const MutationMeal = () => {
+  const [meal, setMeal] = useState({
+    time: '',
+    calories: '',
+    description: '',
+  });
+
+  const changeMeal = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const {name, value} = event.target;
+    setMeal(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
-    <div>
-      MutationMeal is here!
-    </div>
+    <form>
+      <h4>Add / Edit</h4>
+      <div className="form-group">
+        <label htmlFor="time">Select Calories</label>
+        <select
+          name="time"
+          id="time"
+          className="form-select"
+          value={meal.time}
+          onChange={changeMeal}
+          required
+        >
+          <option value="Breakfast">Breakfast</option>
+          <option value="Snack">Snack</option>
+          <option value="Lunch">Lunch</option>
+          <option value="Dinner">Dinner</option>
+        </select>
+        <div className="form-group">
+          <label htmlFor="time">Meal description</label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            required
+            className="form-control"
+            value={meal.description}
+            onChange={changeMeal}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="calories">Calories</label>
+          <input
+            type="number"
+            name="calories"
+            id="calories"
+            required
+            className="form-control"
+            value={meal.calories}
+            onChange={changeMeal}
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn btn-primary mt-3">Create</button>
+    </form>
+
   );
 };
 
